5e0b2c10eff0b90415bd896e332af43b141896ce
Ricky Eckert <ricky_eckert@yahoo.com>
2022-10-27 15:10:28 -0700

    added responses for each food

--- js/food.js
+++ js/food.js
@@ -2,15 +2,15 @@ export {randFoodNumb};
 
 const food1 = {
     name: "BJ's Restaurant & Brewhouse",
-    response: '',
+    response: "Nothing to expensive.",
 };
 const food2 = {
     name: 'Buffalo Wild Wings',
-    response: '',
+    response: 'Their service sucks.',
 };
 const food3 = {
     name: "Chili's Grill & Bar",
-    response: '',
+    response: "But that's too far of a drive...",
 };
 const food4 = {
     name: "Chipotle Mexican Grill",
@@ -18,50 +18,50 @@ const food4 = {
 };
 const food5 = {
     name: "Denny's",
-    response: '',
+    response: 'No... not right now. Maybe tomorrow.',
 };
 const food6 = {
     name: "Fleming's Prime Steakhouse & Wine Bar",
-    response: '',
+    response: "I don't want to get all ready just for a meal.",
 };
 const food7 = {
     name: 'In-N-Out Burger',
-    response: '',
+    response: "I'm not feeling that.",
 };
 const food8 = {
     name: 'Libelula',
-    response: '',
+    response: "That's TOO healthy.",
 };
 const food9 = {
     name: "Lola's Ricos Tacos",
-    response: '',
+    response: "My stomach can't handle that tonight.",
 };
 const food10 = {
     name: "McDonald's",
-    response: '',
+    response: "I don't want fast food.",
 };
 const food11 = {
     name: 'Ming Restaurant',
-    response: '',
+    response: 'mmm...',
 };
 const food12 = {
     name: 'Pizza Hut',
-    response: '',
+    response: 'Too unhealthy.',
 };
 const food13 = {
     name: 'Subway',
-    response: '',
+    response: "I'm not feeling that.",
 };
 const food14 = {
     name: 'Taco Bell',
-    response: '',
+    response: "We've been having that a lot lately.",
 };
 const food15 = {
     name: 'Wasabi Off the Hook',
-    response: '',
+    response: 'Lasttime that made me sick.',
 };
 
 const foodLocations = [food1,food2,food3,food4,food5,food6,food7,food8,food9,food10,food11,food12,food13,food14,food15];
 
 let randFoodNumb = Math.floor(Math.random() * foodLocations.length);
-console.log(randFoodNumb);
+
